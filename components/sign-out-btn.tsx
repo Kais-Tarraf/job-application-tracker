a1f78be47a4ded65e02ce9b3af1fadc82c081597
Kais Tarraf <hossamtr184@gmail.com>
2026-04-02 21:15:36 +0300

    feat: add session log out

---
+++ components/sign-out-btn.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { signOut } from "@/lib/auth/auth-client";
+
+const SignOut = () => {
+	const router = useRouter();
+	return (
+		<DropdownMenuItem
+			onClick={async () => {
+				const result = await signOut();
+				if (result.data) {
+					router.push("/sign-in");
+				} else {
+					alert("Error signing out");
+				}
+			}}
+		>
+			Log Out
+		</DropdownMenuItem>
+	);
+};
+
+export default SignOut;
